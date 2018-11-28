e2a89a01bb3aaa5a579749a6ac86966fd3b6822a
Lillian N <ngo.lilli94@gmail.com>
2018-11-27 22:19:55 -0500

    code for random word added

--- hangman.py
+++ hangman.py
@@ -5,7 +5,7 @@ categories = {
     "countries": ["canada", "namibia", "peru", "switzerland", "taiwan"],
     "fruits": ["cherry", "kiwi", "mango", "papaya", "strawberry"],
     "games": ["chess", "monopoly", "operation", "risk", "scrabble"],
-    "movies": ["casablanca", "frozen", "jaws", "hairspray", "titanic"]
+    "movies": ["casablanca", "frozen", "inception", "jaws", "titanic"]
 }
 
 # user_choice = raw_input("Please choose a category: ")
@@ -17,10 +17,13 @@ categories = {
 
 
 def hangman():
-    print "Hello! Welcome to Python Hangman! We have five categories of words: animals, countries, fruits, games, and movies."
+    print "Hello! Welcome to Python Hangman! There are five categories of words: animals, countries, fruits, games, and movies."
     user_choice = raw_input("To get started, please choose one of the available categories: ")
     if user_choice.lower() in categories.keys():
-        print "Okay"
+        user_category = categories[user_choice]
+        hidden_word = random.choice(user_category)
+        guessed_letters = []
+        
     else:
         print "Sorry that is not one of the categories"
         hangman()
