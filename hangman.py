7f4a4a32dc1f24df477aef439194afbb83217dfa
Lillian N <ngo.lilli94@gmail.com>
2018-11-27 20:14:25 -0500

    Word categories added

---
+++ hangman.py
@@ -0,0 +1,14 @@
+import random
+
+categories = {
+    "animals": ["alpaca", "chinchilla", "dolphin", "orca", "rabbit"],
+    "countries": ["canada", "namibia", "peru", "switzerland", "taiwan"],
+    "fruits": ["cherry", "kiwi", "mango", "papaya", "strawberry"],
+    "games": ["chess", "monopoly", "operation", "risk", "scrabble"],
+    "movies": ["casablanca", "frozen", "jaws", "hairspray", "titanic"]
+}
+
+# user_choice = raw_input("Please choose a category: ")
+# user_category = categories[user_choice]
+# hidden_word = random.choice(user_category)
+# print hidden_word
